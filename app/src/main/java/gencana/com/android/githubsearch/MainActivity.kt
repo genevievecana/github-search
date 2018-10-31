093df52805ffac268bd79cf31792db536756c94b
Gen Cana <yan2xcana@gmail.com>
2018-11-01 00:08:07 +0800

    added dependencies

--- app/src/main/java/gencana/com/android/githubsearch/MainActivity.kt
+++ app/src/main/java/gencana/com/android/githubsearch/MainActivity.kt
@@ -1,7 +1,7 @@
 package gencana.com.android.githubsearch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
